09de2786e9a9c1d145f9481e358f095d6b16898e
Ganga0048 <katwalganga850@gmail.com>
2023-10-05 13:00:09 +0200

    tessst

--- main.py
+++ main.py
@@ -4,9 +4,8 @@ int_player = input("How many players are playing ?")
 
 class Hero:
 
-    def hello_hero():
-        print("Hello from hero")
 
 
 if __name__ == "__main__":
+    Hero.hello_hero() 
     Hero.hello_hero()
